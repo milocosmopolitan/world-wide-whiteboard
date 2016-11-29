b33b14616566b7b3aeb1fa938b453d5fabf5bacf
milocosmopolitan <milocosmopolitan@gmail.com>
2016-11-29 16:31:54 -0500

    extra credit

--- browser/app.js
+++ browser/app.js
@@ -1,13 +1,70 @@
-var socket = io(window.location.origin);
+console.log(window.location)
 
-socket.on('connect', function(){
-	console.log('made a persistent tow-way connection to server');
-	socket.on('draw', function(...payload) {
 
-		console.log('drawing broadcast')
-		whiteboard.draw(...payload);
+
+
+if(window.location.pathname !== '/'){
+	var turing = io(window.location.pathname);
+	turing.on('connect', function(){
+		console.log('turing connect');
+		turing.on('draw', function(...payload) {
+
+			console.log('drawing broadcast')
+			whiteboard.draw(...payload);
+		})
 	})
-})
+
+	whiteboard.on('draw', function(...payload){
+		console.log(...payload);
+		turing.emit('draw', ...payload);
+	})
+
+	turing.on('draw', function(...payload) {	
+		whiteboard.draw(...payload);
+		console.log('drawing broadcast')	
+	})	
+
+	turing.on('restore', function(payload) {
+		console.log('restore');
+		whiteboard.draw(payload[0], payload[1], payload[2]);
+		
+	})
+} else {
+	var socket = io(window.location.origin);
+	socket.on('connect', function(){
+		console.log('made a persistent tow-way connection to server');
+		socket.on('draw', function(...payload) {
+
+			console.log('drawing broadcast')
+			whiteboard.draw(...payload);
+		})
+	})
+
+	whiteboard.on('draw', function(...payload){
+		console.log(...payload);
+		socket.emit('draw', ...payload);
+	})
+
+
+	socket.on('draw', function(...payload) {	
+		whiteboard.draw(...payload);
+		console.log('drawing broadcast')	
+	})
+
+	socket.on('restore', function(payload) {
+
+		console.log('restore');
+		console.log(payload);
+		whiteboard.draw(payload[0], payload[1], payload[2]);
+		
+	})
+}
+
+
+//var turing = io('/turing');
+
+
+
 
 // setTimeout(function(){
 // 	socket.disconnect();
@@ -23,15 +80,15 @@ whiteboard.on('draw', function(...payload){
 })
 
 
-socket.on('restore', function(payload) {
+// socket.on('restore', function(payload) {
 
-	console.log('restore');
-	console.log(payload);
-	whiteboard.draw(payload[0], payload[1], payload[2]);
+// 	console.log('restore');
+// 	console.log(payload);
+// 	whiteboard.draw(payload[0], payload[1], payload[2]);
 	
-})
+// })
 
-socket.on('draw', function(...payload) {	
-	whiteboard.draw(...payload);
-	console.log('drawing broadcast')	
-})
+// socket.on('draw', function(...payload) {	
+// 	whiteboard.draw(...payload);
+// 	console.log('drawing broadcast')	
+// })
--- server.js
+++ server.js
@@ -12,36 +12,76 @@ server.on('request', app);
 
 var io = socketio(server);
 
-//var allClients = [];
+var clients = {
+	all: [],
+	turing: []
+};
 
-var drawing = []
+var drawing = {
+	default: [],
+	turing: [],
+	grace: [],
+	kitchen: []
+}
+
+
+var turing = io.of('/turing');
+
+
 
 io.on('connection', function (socket) {
     console.log('A new client has connected!');
-    console.log(socket);    
+    //console.log(socket);    
+    clients.all.push(socket);
 
-    drawing.forEach(data=>{
-    	console.log('data', data)
+    if(clients.all.length > 1) {
+    	console.log('/Drawing', drawing);
+    	drawing.default.forEach(data=>{	    	
+	    	socket.emit('restore', data);
+	    })
+    }
 
-    	// console.log('start', data)
-    	// console.log('end', data[1])
-    	// console.log('stroke', data[2])
-    	socket.emit('restore', data);
-    })
-    
-    
     socket.on('disconnect', function() {
-    	// var i = allClients.indexOf(socket);
-     //  	delete allClients[i];      	
+    	var i = clients.all.indexOf(socket);
+      	delete clients.all[i];      	
     	console.log('user disconnected');            
    });
 
     socket.on('draw', function(...data){
     	let payload = data;
-    	drawing.push(payload);
+    	drawing.default.push(payload);
     	socket.broadcast.emit('draw', ...data);    	
     })
 });
+
+turing.on('connection', function (socket) {
+    console.log('This is suppose to be turing');
+    //console.log(socket);    
+    clients.turing.push(socket);
+
+    if(clients.turing.length > 1) {
+    	console.log('/Turing Drawing', drawing);
+    	drawing.turing.forEach(data=>{	    	
+	    	socket.emit('restore', data);
+	    })
+    }
+
+    socket.on('disconnect', function() {
+    	var i = clients.turing.indexOf(socket);
+      	delete clients.turing[i];      	
+    	console.log('user disconnected');            
+   });
+
+    socket.on('draw', function(...data){    	
+    	console.log('turing drawing')
+    	let payload = data;
+    	drawing.turing.push(payload);
+
+    	//console.log(drawing);
+    	socket.broadcast.emit('draw', ...data);    	
+    })
+});
+
 
 server.listen(1337, function () {
     console.log('The server is listening on port 1337!');
@@ -49,6 +89,21 @@ server.listen(1337, function () {
 
 app.use(express.static(path.join(__dirname, 'browser')));
 
+app.get('/turing', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+
+
+app.get('/grace', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/kitchen', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
